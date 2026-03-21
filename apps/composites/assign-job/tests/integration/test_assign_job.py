47cfd36193864c8df3cd3831a42a0ea79be62dd5
TAN SUAN LOONG <suanloongtan@gmail.com>
2026-03-22 00:12:40 +0800

    test(assign-job): unit tests passing

--- apps/composites/assign-job/tests/integration/test_assign_job.py
+++ apps/composites/assign-job/tests/integration/test_assign_job.py
@@ -1,2 +1,5 @@
-def test_1_plus_1():
-  assert 1 + 1 == 2
+# tests/integration/test_assign_job.py
+# TODO: Integration tests require RabbitMQ and dependent services running
+# Will test:
+# - Full assign_contractor flow with mocked HTTP calls
+# - Consumer processes Case_Opened message correctly
